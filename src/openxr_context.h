dac128e0eb07973d0eee5d12526ee1c368e8ce61
PZerua <pachus.wtf@gmail.com>
2023-05-24 17:15:27 +0200

    ups

--- src/openxr_context.h
+++ src/openxr_context.h
@@ -16,6 +16,9 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_LEFT_HANDED
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 // small helper so we don't forget whether we treat 0 as left or right hand
 enum OPENXR_HANDS
