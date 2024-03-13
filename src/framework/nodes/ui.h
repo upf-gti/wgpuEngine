e9d7838f66d312e861045638ce8aa777390b62ae
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2024-03-13 12:24:17 +0100

    Added default constructor to UI elements

--- src/framework/nodes/ui.h
+++ src/framework/nodes/ui.h
@@ -19,13 +19,14 @@ namespace ui {
     class Panel2D : public Node2D {
     public:
 
-        Color color;
+        Color color = glm::vec4(0.0f);
 
         bool render_background  = true;
         bool centered           = false;
 
         MeshInstance quad_mesh;
 
+        Panel2D() {};
         Panel2D(const std::string& name, const glm::vec2& p, const glm::vec2& s, const Color& c = colors::WHITE);
 
         void set_color(const Color& c);
@@ -39,9 +40,10 @@ namespace ui {
     class Container2D : public Panel2D {
     public:
 
-        glm::vec2 padding;
-        glm::vec2 item_margin;
+        glm::vec2 padding = { 0.0f, 0.0f };
+        glm::vec2 item_margin = { 0.0f, 0.0f };
 
+        Container2D() {};
         Container2D(const std::string& name, const glm::vec2& p, const Color& c = colors::WHITE);
 
         void on_children_changed() override;
@@ -49,7 +51,7 @@ namespace ui {
 
     class HContainer2D : public Container2D {
     public:
-
+        HContainer2D() {};
         HContainer2D(const std::string& name, const glm::vec2& p, const Color& c = colors::WHITE);
 
         void on_children_changed() override;
@@ -57,7 +59,7 @@ namespace ui {
 
     class VContainer2D : public Container2D {
     public:
-
+        VContainer2D() {};
         VContainer2D(const std::string& name, const glm::vec2& p, const Color& c = colors::WHITE);
 
         void on_children_changed() override;
@@ -68,6 +70,7 @@ namespace ui {
 
         TextEntity* text_entity = nullptr;
 
+        Text2D() {};
         Text2D(const std::string& _text, const glm::vec2& pos, float scale = 1.f, const Color& color = colors::WHITE);
 
         void set_text(const std::string& text) { text_entity->set_text(text); };
@@ -100,6 +103,7 @@ namespace ui {
         bool disabled               = false;
         bool is_color_button        = true;
 
+        Button2D() {};
         Button2D(const std::string& sg, const Color& color = colors::WHITE, uint8_t parameter_flags = 0);
         Button2D(const std::string& sg, uint8_t parameter_flags, const glm::vec2& pos, const glm::vec2& size = glm::vec2(BUTTON_SIZE));
         Button2D(const std::string& sg, const Color& color, uint8_t parameter_flags, const glm::vec2& pos, const glm::vec2& size = glm::vec2(BUTTON_SIZE));
@@ -159,6 +163,7 @@ namespace ui {
         float max_value = 1.0f;
         float step_value = 0.0f;
 
+        Slider2D() {};
         Slider2D(const std::string& sg, float v, int mode = SliderMode::VERTICAL, float min = 0.0f, float max = 1.0f, float step = 0.0f);
         Slider2D(const std::string& sg, float v, const glm::vec2& pos, const glm::vec2& size = glm::vec2(BUTTON_SIZE), int mode = SliderMode::VERTICAL, float min = 0.0f, float max = 1.0f, float step = 0.0f);
 
@@ -172,7 +177,7 @@ namespace ui {
 
         std::string signal;
 
-
+        ColorPicker2D() {};
         ColorPicker2D(const std::string& sg, const Color& c, bool skip_intensity = false);
         ColorPicker2D(const std::string& sg, const glm::vec2& p, const glm::vec2& s, const Color& c, bool skip_intensity = false);
 
