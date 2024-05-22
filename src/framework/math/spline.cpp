e30263b79488793c0613eeff4b33440eae8585eb
jxarco <alexroco.30@gmail.com>
2024-05-22 03:55:24 +0200

    Use knots (pos, size) insted of vec3 for spline points

--- src/framework/math/spline.cpp
+++ src/framework/math/spline.cpp
@@ -2,37 +2,37 @@
 
 // Splines
 
-void Spline::add_point(const glm::vec3& new_point)
+void Spline::add_knot(const Knot& new_knot)
 {
-    points.push_back(new_point);
+    knots.push_back(new_knot);
 }
 
 // Bezier splines
 
-glm::vec3 BezierSpline::evaluate_quadratic(float t, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2) const
+Knot BezierSpline::evaluate_quadratic(float t, const Knot& p0, const Knot& p1, const Knot& p2) const
 {
     float u = 1.0f - t;
     return (u * u) * p0 + 2.0f * u * t * p1 + (t * t) * p2;
 }
 
-glm::vec3 BezierSpline::evaluate_cubic(float t, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3) const
+Knot BezierSpline::evaluate_cubic(float t, const Knot& p0, const Knot& p1, const Knot& p2, const Knot& p3) const
 {
     float u = 1.0f - t;
     return (u * u * u) * p0 + 3.0f * (u * u) * t * p1 + 3.0f * u * (t * t) * p2 + (t * t * t) * p3;
 }
 
-std::vector<glm::vec3> BezierSpline::construct_target_vector(uint32_t n)
+std::vector<Knot> BezierSpline::construct_target_vector(uint32_t n)
 {
-    std::vector<glm::vec3> result;
+    std::vector<Knot> result;
     result.resize(n);
 
-    result[0] = points.at(0) + 2.0f * points.at(1);
+    result[0] = knots.at(0) + 2.0f * knots.at(1);
 
     for (int i = 1; i < n - 1; i++) {
-        result[i] = (points.at(i) * 2.0f + points.at(i + 1)) * 2.0f;
+        result[i] = (knots.at(i) * 2.0f + knots.at(i + 1)) * 2.0f;
     }
 
-    result[result.size() - 1] = points.at(n - 1) * 8.0f + points.at(n);
+    result[result.size() - 1] = knots.at(n - 1) * 8.0f + knots.at(n);
 
     return result;
 }
@@ -79,17 +79,17 @@ std::vector<float> BezierSpline::construct_upper_diagonal_vector(uint32_t length
     return result;
 }
 
-std::vector<glm::vec3> BezierSpline::compute_control_points(uint32_t segments)
+std::vector<Knot> BezierSpline::compute_control_points(uint32_t segments)
 {
-    std::vector<glm::vec3> result;
+    std::vector<Knot> result;
     result.resize(2 * segments);
 
-    std::vector<glm::vec3> target = construct_target_vector(segments);
+    std::vector<Knot> target = construct_target_vector(segments);
     std::vector<float> lowerDiag = construct_lower_diagonal_vector(segments - 1);
     std::vector<float> mainDiag = construct_main_diagonal_vector(segments);
     std::vector<float> upperDiag = construct_upper_diagonal_vector(segments - 1);
 
-    std::vector<glm::vec3> new_target;
+    std::vector<Knot> new_target;
     new_target.resize(segments);
     std::vector<float> new_upper_diag;
     new_upper_diag.resize(segments - 1u);
@@ -116,24 +116,24 @@ std::vector<glm::vec3> BezierSpline::compute_control_points(uint32_t segments)
 
     // calculate remaining control points c_i,1 directly:
     for (uint32_t i = 0; i < segments - 1u; i++) {
-        result[segments + i] = points[i + 1] * 2.0f - result[i + 1];
+        result[segments + i] = knots[i + 1] * 2.0f - result[i + 1];
     }
 
-    result[2u * segments - 1u] = (points[segments] + result[segments - 1u]) * 0.5f;
+    result[2u * segments - 1u] = (knots[segments] + result[segments - 1u]) * 0.5f;
 
     return result;
 }
 
-void BezierSpline::for_each(std::function<void(const glm::vec3&)> fn)
+void BezierSpline::for_each(std::function<void(const Knot&)> fn)
 {
-    int n = count();
+    int n = knots.size();
     if (!n) {
         return;
     }
 
-    uint32_t segments = points.size() - 1;
+    uint32_t segments = knots.size() - 1;
 
-    glm::vec3 start_point = points[0];
+    Knot start_point = knots[0];
 
     if (!segments) {
         fn(start_point);
@@ -142,21 +142,22 @@ void BezierSpline::for_each(std::function<void(const glm::vec3&)> fn)
     else if (segments == 1) {
         for (int j = 0; j < density; ++j) {
             float t = static_cast<float>(j) / (density - 1);
-            const glm::vec3& end_point = points[1];
-            fn(glm::mix(start_point, end_point, t));
+            const Knot& end_point = knots[1];
+            const Knot& point = start_point * (1.0f - t) + end_point * t;
+            fn(point);
         }
     }
 
     // Smooth path using bezier segments
     else {
-        const std::vector<glm::vec3>& control_points = compute_control_points(segments);
+        const std::vector<Knot>& control_points = compute_control_points(segments);
 
         for (uint32_t i = 0; i < segments; i++) {
-            const glm::vec3& end_point = points.at(i + 1);
+            const Knot& end_point = knots.at(i + 1);
 
             for (int j = 0; j < density; ++j) {
                 float t = static_cast<float>(j) / (density - 1u);
-                const glm::vec3& point = evaluate_cubic(t, start_point, control_points[i], control_points[segments + i], end_point);
+                const Knot& point = evaluate_cubic(t, start_point, control_points[i], control_points[segments + i], end_point);
                 fn(point);
             }
 
@@ -167,89 +168,89 @@ void BezierSpline::for_each(std::function<void(const glm::vec3&)> fn)
 
 // B-splines
 
-float BSpline::get_basis(int i, int k, float t, const std::vector<float>& knots) const
-{
-    if (k == 1) {
-        return (knots[i] <= t && t < knots[i + 1]) ? 1.0f : 0.0f;
-    }
-    else {
-        float denom1 = knots[i + k - 1] - knots[i];
-        float denom2 = knots[i + k] - knots[i + 1];
-        float term1 = denom1 == 0 ? 0 : (t - knots[i]) / denom1 * get_basis(i, k - 1, t, knots);
-        float term2 = denom2 == 0 ? 0 : (knots[i + k] - t) / denom2 * get_basis(i + 1, k - 1, t, knots);
-        return term1 + term2;
-    }
-}
-
-glm::vec3 BSpline::evaluate(float t) const
-{
-    glm::vec3 point(0.0f);
-    int n = points.size();
-    for (int i = 0; i < n; ++i) {
-        float basis = get_basis(i, degree + 1, t, knots);
-        point += basis * points[i];
-    }
-    return point;
-}
-
-void BSpline::for_each(std::function<void(const glm::vec3&)> fn)
-{
-    if (count() < (degree + 1)) {
-        return;
-    }
-
-    float step = 1.f / (density - 1u);
-
-    for (int i = 0; i < density; ++i) {
-        float t = i * step;
-        glm::vec3 point = evaluate(t);
-        fn(point);
-    }
-}
-
-void BSpline::add_point(const glm::vec3& new_point)
-{
-    points.push_back(new_point);
-
-    // Knot vector (uniform in this case)
-    int control_points_count = points.size();
-    knots.clear();
-    knots.resize(control_points_count + degree + 1);
-
-    for (int i = 0; i < knots.size(); ++i) {
-        knots[i] = static_cast<float>(i) / (knots.size() - 1);
-    }
-}
+//float BSpline::get_basis(int i, int k, float t, const std::vector<float>& knots) const
+//{
+//    if (k == 1) {
+//        return (knots[i] <= t && t < knots[i + 1]) ? 1.0f : 0.0f;
+//    }
+//    else {
+//        float denom1 = knots[i + k - 1] - knots[i];
+//        float denom2 = knots[i + k] - knots[i + 1];
+//        float term1 = denom1 == 0 ? 0 : (t - knots[i]) / denom1 * get_basis(i, k - 1, t, knots);
+//        float term2 = denom2 == 0 ? 0 : (knots[i + k] - t) / denom2 * get_basis(i + 1, k - 1, t, knots);
+//        return term1 + term2;
+//    }
+//}
+//
+//glm::vec3 BSpline::evaluate(float t) const
+//{
+//    glm::vec3 point(0.0f);
+//    int n = points.size();
+//    for (int i = 0; i < n; ++i) {
+//        float basis = get_basis(i, degree + 1, t, knots);
+//        point += basis * points[i];
+//    }
+//    return point;
+//}
+//
+//void BSpline::for_each(std::function<void(const glm::vec3&)> fn)
+//{
+//    if (count() < (degree + 1)) {
+//        return;
+//    }
+//
+//    float step = 1.f / (density - 1u);
+//
+//    for (int i = 0; i < density; ++i) {
+//        float t = i * step;
+//        glm::vec3 point = evaluate(t);
+//        fn(point);
+//    }
+//}
+//
+//void BSpline::add_point(const glm::vec3& new_point)
+//{
+//    points.push_back(new_point);
+//
+//    // Knot vector (uniform in this case)
+//    int control_points_count = points.size();
+//    knots.clear();
+//    knots.resize(control_points_count + degree + 1);
+//
+//    for (int i = 0; i < knots.size(); ++i) {
+//        knots[i] = static_cast<float>(i) / (knots.size() - 1);
+//    }
+//}
 
 // NURBS
 
-glm::vec3 NURBS::evaluate(float t) const
-{
-    t *= (knots.back() - knots.front()) + knots.front();
-
-    glm::vec3 numerator(0.0f);
-    float denominator = 0.0f;
-    int n = points.size();
-    for (int i = 0; i < n; ++i) {
-        float basis = get_basis(i, degree + 1, t, knots) * weights[i];
-        numerator += basis * points[i];
-        denominator += basis;
-    }
-    return numerator / denominator;
-}
-
-void NURBS::add_weighted_point(const glm::vec3& new_point, float weight)
-{
-    points.push_back(new_point);
-
-    weights.push_back(weight);
-
-    uint32_t d = degree + 1;
-    int control_points_count = points.size();
-    knots.clear();
-    knots.resize(control_points_count + d);
-
-    for (int i = 0; i < knots.size(); ++i) {
-        knots[i] = i < d ? 0.0f : (i >= (knots.size() - d) ? 1.0f : static_cast<float>(i) / (knots.size() - 1));
-    }
-}
+//glm::vec3 NURBS::evaluate(float t) const
+//{
+//    t *= (knots.back() - knots.front()) + knots.front();
+//
+//    glm::vec3 numerator(0.0f);
+//    float denominator = 0.0f;
+//    int n = points.size();
+//    for (int i = 0; i < n; ++i) {
+//        float basis = get_basis(i, degree + 1, t, knots) * weights[i];
+//        numerator += basis * points[i];
+//        denominator += basis;
+//    }
+//    return numerator / denominator;
+//}
+//
+//void NURBS::add_weighted_point(const glm::vec3& new_point, float weight)
+//{
+//    points.push_back(new_point);
+//
+//    weights.push_back(weight);
+//
+//    uint32_t d = degree + 1;
+//    int control_points_count = points.size();
+//    knots.clear();
+//    knots.resize(control_points_count + d);
+//
+//    for (int i = 0; i < knots.size(); ++i) {
+//        knots[i] = i < d ? 0.0f : (i >= (knots.size() - d) ? 1.0f : static_cast<float>(i) / (knots.size() - 1));
+//    }
+//}
--- src/framework/math/spline.h
+++ src/framework/math/spline.h
@@ -7,13 +7,16 @@
 
 struct Knot {
     glm::vec3 position;
-    float size = 0.f;
-
-    inline Knot operator+(const Knot& knot) const { return { position + knot.position }; }
-    inline Knot operator-(const Knot& knot) const { return { position - knot.position }; }
-    inline Knot operator*(const Knot& knot) const { return { position * knot.position }; }
-
+    float size = 1.f;
 };
+
+inline Knot operator+(const Knot& a, const Knot& b) { return { a.position + b.position, a.size + b.size }; }
+inline Knot operator-(const Knot& a, const Knot& b) { return { a.position - b.position, a.size - b.size }; }
+inline Knot operator*(const Knot& a, const Knot& b) { return { a.position * b.position, a.size * b.size }; }
+inline Knot operator/(const Knot& a, const Knot& b) { return { a.position / b.position, a.size / b.size }; }
+
+inline Knot operator*(float scalar, const Knot& b) { return { scalar * b.position, scalar * b.size }; }
+inline Knot operator*(const Knot& a, float scalar) { return { a.position * scalar, a.size * scalar }; }
 
 class Spline {
 
@@ -21,32 +24,32 @@ protected:
 
     uint32_t density = 48;
 
-    std::vector<glm::vec3> points;
+    std::vector<Knot> knots;
 
-    virtual glm::vec3 evaluate(float t) const { return {}; };
+    virtual Knot evaluate(float t) const { return {}; };
 
 public:
 
     Spline() {};
 
-    virtual void add_point(const glm::vec3& new_point);
+    virtual void add_knot(const Knot& new_knot);
 
-    virtual void for_each(std::function<void(const glm::vec3&)> fn) {}
+    virtual void for_each(std::function<void(const Knot&)> fn) {}
 
-    uint32_t count() const { return points.size(); }
+    uint32_t count() const { return knots.size(); }
 
     void set_density(uint32_t new_density) { density = new_density; }
 };
 
 struct BezierSpline : public Spline {
 
-    glm::vec3 evaluate_quadratic(float t, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2) const;
+    Knot evaluate_quadratic(float t, const Knot& p0, const Knot& p1, const Knot& p2) const;
 
-    glm::vec3 evaluate_cubic(float t, const glm::vec3& p0, const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3) const;
+    Knot evaluate_cubic(float t, const Knot& p0, const Knot& p1, const Knot& p2, const Knot& p3) const;
 
-    std::vector<glm::vec3> compute_control_points(uint32_t segments);
+    std::vector<Knot> compute_control_points(uint32_t segments);
 
-    std::vector<glm::vec3> construct_target_vector(uint32_t n);
+    std::vector<Knot> construct_target_vector(uint32_t n);
     std::vector<float> construct_lower_diagonal_vector(uint32_t length);
     std::vector<float> construct_main_diagonal_vector(uint32_t n);
     std::vector<float> construct_upper_diagonal_vector(uint32_t length);
@@ -55,52 +58,53 @@ public:
 
     BezierSpline() {
 
-        density = 12;
+        density = 24;
 
-        points = {
-            glm::vec3(0.0f, 0.404f, 0.0f) * 0.5f,
-            glm::vec3(0.0f, -0.068f, 0.0f) * 0.5f,
-            glm::vec3(0.205f, 0.344f, 0.0f) * 0.5f,
-            glm::vec3(0.324f, 0.074f, 0.0f) * 0.5f,
-            glm::vec3(0.43f, 0.2f, 0.0f) * 0.5f,
-            glm::vec3(0.58f, 0.467f, 0.0f) * 0.5f,
-            glm::vec3(0.882f, 0.31f, 0.0f) * 0.5f
+        knots = {
+            Knot({ 0.0f, 0.404f, 0.0f }, 2.0f) * 0.5f,
+            Knot({ 0.0f, -0.068f, 0.0f }) * 0.5f,
+            Knot({ 0.205f, 0.344f, 0.0f }) * 0.5f,
+            Knot({ 0.324f, 0.074f, 0.0f }, 2.0f) * 0.5f,
+            Knot({ 0.43f, 0.2f, 0.0f }) * 0.5f,
+            Knot({ 0.58f, 0.467f, 0.0f }) * 0.5f,
+            Knot({ 0.882f, 0.31f, 0.0f }, 2.0) * 0.5f,
+            Knot({ 0.682f, 0.121f, 0.0f }, 2.0) * 0.5f
         };
     }
 
-    void for_each(std::function<void(const glm::vec3&)> fn) override;
+    void for_each(std::function<void(const Knot&)> fn) override;
 };
 
-class BSpline : public Spline {
-
-protected:
-
-    uint32_t degree = 3;
-
-    std::vector<float> knots;
-
-    float get_basis(int i, int k, float t, const std::vector<float>& knots) const;
-
-    glm::vec3 evaluate(float t) const override;
-
-public:
-
-    BSpline() {};
-
-    void add_point(const glm::vec3& new_point) override;
-
-    void for_each(std::function<void(const glm::vec3&)> fn) override;
-};
-
-class NURBS : public BSpline {
-
-    std::vector<float> weights;
-
-    glm::vec3 evaluate(float t) const override;
-
-public:
-
-    NURBS() {};
-
-    void add_weighted_point(const glm::vec3& new_point, float weight);
-};
+//class BSpline : public Spline {
+//
+//protected:
+//
+//    uint32_t degree = 3;
+//
+//    std::vector<float> knots;
+//
+//    float get_basis(int i, int k, float t, const std::vector<float>& knots) const;
+//
+//    glm::vec3 evaluate(float t) const override;
+//
+//public:
+//
+//    BSpline() {};
+//
+//    void add_point(const glm::vec3& new_point) override;
+//
+//    void for_each(std::function<void(const glm::vec3&)> fn) override;
+//};
+//
+//class NURBS : public BSpline {
+//
+//    std::vector<float> weights;
+//
+//    glm::vec3 evaluate(float t) const override;
+//
+//public:
+//
+//    NURBS() {};
+//
+//    void add_weighted_point(const glm::vec3& new_point, float weight);
+//};
