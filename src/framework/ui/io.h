e889ce42a226fbcd63c8999e7c39807ffcb39ffb
jxarco <alexroco.30@gmail.com>
2024-06-14 00:05:08 +0200

    move keyboard to rooms

--- src/framework/ui/io.h
+++ src/framework/ui/io.h
@@ -2,47 +2,15 @@
 
 #include "includes.h"
 
-#include "framework/nodes/node.h"
+#include <string>
+#include <vector>
+
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
 
 class Node2D;
-class MeshInstance3D;
-class Viewport3D;
-
-namespace ui {
-    class Text2D;
-}
 
 class IO {
-
-    struct XrKey {
-        std::string label = "";
-        glm::vec2 position = { 0.0f, 0.0f };
-        uint32_t flags = 0;
-        std::string texture_path = "";
-        glm::vec2 size = glm::vec2(42.0f);
-        bool bind_event = true;
-    };
-
-    struct XrKeyboardState {
-        bool caps = false;
-        bool caps_locked = false;
-        bool symbols = false;
-        std::string input = ":";
-        ui::Text2D* text = nullptr;
-
-        const std::string& get_input() { return input; }
-        void clear_input();
-        void push_char(char c);
-        void remove_char();
-        void reset();
-
-        void toggle_caps();
-        void toggle_caps_lock();
-        void toggle_symbols();
-        void disable_caps();
-    };
-
-    static XrKeyboardState xr_keyboard_state;
 
     static float xr_ray_distance;
 
@@ -51,13 +19,6 @@ class IO {
 
     static glm::vec2 xr_position;
     static glm::vec3 xr_world_position;
-
-    static Node2D* keyboard_2d;
-    static Viewport3D* xr_keyboard;
-    static bool keyboard_active;
-
-    static void create_keyboard_letters_layout(std::vector<IO::XrKey>& keys, float start_x, float start_y, float margin);
-    static void create_keyboard_symbols_layout(std::vector<IO::XrKey>& keys, float start_x, float start_y, float margin);
 
 public:
 
@@ -94,7 +55,4 @@ public:
     static const glm::vec2& get_xr_position() { return xr_position; }
     static const glm::vec3& get_xr_world_position() { return xr_world_position; }
     static const float get_xr_ray_distance() { return xr_ray_distance; }
-
-    static void request_keyboard() { keyboard_active = true; };
-    static void close_keyboard() { keyboard_active = false; };
 };
