926ad48cf6b4cfddeb73b3de51b674f6247f3251
PZerua <pachus.wtf@gmail.com>
2024-09-16 17:37:12 +0200

    wip: improve imgui panel

--- src/framework/nodes/mesh_instance_3d.cpp
+++ src/framework/nodes/mesh_instance_3d.cpp
@@ -29,3 +29,24 @@ void MeshInstance3D::update(float delta_time)
 {  
     Node3D::update(delta_time);
 }
+
+void MeshInstance3D::render_gui()
+{
+    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(ImColor(230, 150, 50)));
+    bool is_open = ImGui::TreeNodeEx("Surfaces");
+    ImGui::PopStyleColor();
+
+    if (is_open)
+    {
+        for (int i = 0; i < surfaces.size(); ++i) {
+            std::string surface_name = surfaces[i]->get_name();
+            std::string final_surface_name = surface_name.empty() ? ("Surface " + std::to_string(i)).c_str() : surface_name;
+            ImGui::TreeNodeEx(final_surface_name.c_str(), ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_Leaf);
+            ImGui::TreePop();
+        }
+
+        ImGui::TreePop();
+    }
+
+    Node3D::render_gui();
+}
